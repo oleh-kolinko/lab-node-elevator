95288e10fdf7ef3fa47f64bce220c509f66369b7
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-02-20 23:36:22 -0500

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,125 @@
+
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.passengers = [];
+    this.waitingList = [];
+    this.direction = 'up';
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.intervalID = setInterval(()=>{
+      this.update();
+    },1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalID);
+    console.log('Elevator stoped');
+   }
+
+  update() {
+    this.log();
+
+    this.waitingList.forEach((person)=>{
+      if(person.originFloor === this.floor){
+        this._passengersEnter(person);
+      }
+    });
+
+    this.passengers.forEach((person)=>{
+      if(person.destinationFloor === this.floor){
+        this._passengersLeave(person);
+      }
+    });
+
+
+    if(!this.requests.length){
+      this.stop();
+    }
+
+    if(this.direction === 'up'){
+      this.direction = 'down';
+      this.requests.forEach(req => {
+        if(req > this.floor){
+          this.direction = 'up';
+        }
+      });
+    }else {
+    this.direction = 'down';
+    this.requests.forEach(req => {
+      if(req > this.floor){
+        this.direction = 'up';
+      }
+    });
+    }
+
+
+
+    if(this.direction === 'up'){
+      this.floorUp();
+    }else{
+      this.floorDown();
+    }
+
+  }
+
+  _passengersEnter(person) {
+
+    this.passengers.push(person);
+    this.requests.push(person.destinationFloor);
+
+    //Delete person form waitingList
+    let i = this.waitingList.indexOf(person);
+    if(i > -1 ){
+      console.log(`${person.name} has enter the elevator`);
+      this.waitingList.splice(i,1);
+    }else console.error('No such person in waitingList');
+
+    //Remove request of entered person
+    i = this.requests.indexOf(this.floor);
+    this.requests.splice(i,1);
+  }
+
+  _passengersLeave(person) {
+
+    //Delete person form passengers
+    let i = this.passengers.indexOf(person);
+    if(i > -1 ){
+      console.log(`${person.name} has left the elevator`);
+      this.passengers.splice(i,1);
+    }else console.error('No such person in passengers');
+
+    //Remove request of left person
+    i = this.requests.indexOf(this.floor);
+    this.requests.splice(i,1);
+  }
+
+  floorUp() {
+    if(this.floor < this.MAXFLOOR){
+      this.floor ++;
+    }else{
+      console.log('Reached max floor can\'t go up ');
+    }
+  }
+
+  floorDown() {
+    if(this.floor > 0){
+      this.floor --;
+    }else{
+      console.log('Reached floor 0 can\'t go down ');
+    }
+  }
+  call(person) {
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+  }
+
+  log() {
+    console.log(`Direction : ${this.direction}   |   Current floor : ${this.floor} `);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,13 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const elevator = new Elevator();
+const p1 = new Person('Mary',3,7);
+const p2 = new Person('Sabina',5,2);
+const p3 = new Person('Fernanda',4,8);
+const p4 = new Person('Sabina',3,2);
+
+elevator.start();
+elevator.call(p1);
+elevator.call(p2);
+elevator.call(p3);
